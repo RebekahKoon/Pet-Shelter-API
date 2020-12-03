3d96c37aba4e5ecbc84bd74c545f4a8352a9e764
Rebekah <koonr@oregonstate.edu>
2020-12-03 13:33:16 -0800

    route to get a pet by id

--- src/pets.js
+++ src/pets.js
@@ -37,4 +37,23 @@ router.post('/', async function (req, res) {
   }
 })
 
+/**
+ *
+ */
+router.get('/:pet_id', async function (req, res) {
+  const pet = await api.get_entity_by_id(PET, req.params.pet_id)
+  const accepts = req.accepts(['application/json'])
+
+  if (pet[0] === undefined) {
+    res.status(404).json({ Error: 'No pet with this pet_id exists' })
+  } else if (!accepts) {
+    res.status(406).json({ Error: 'application/json is the only supported content type' })
+  } else {
+    pet[0].id = req.params.pet_id
+    pet[0].self = req.protocol + '://' + req.get('host') + req.originalUrl
+
+    res.status(200).json(pet[0])
+  }
+})
+
 module.exports = router
